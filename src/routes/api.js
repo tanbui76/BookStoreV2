004d7feeb4d856d73ad17736f8889aa229dce155
Bùi Nguyễn Nhật Tân <84611805+tanbui76@users.noreply.github.com>
2023-08-23 15:00:41 +0700

    Chỉnh sửa route

--- src/routes/api.js
+++ src/routes/api.js
@@ -2,6 +2,7 @@ import express from 'express';
 import authController from '../controllers/authController';
 import bookController from '../controllers/bookController';
 import tokenMiddleware from '../middlewares/tokenMiddleware';
+import userMiddleware from '../controllers/userController'
 
 let router = express.Router();
 
@@ -12,6 +13,7 @@ const initAPIRoutes = (app) => {
     router.get('/searchbooks', bookController.searchBook);
     router.get('/getbookbycategory', tokenMiddleware, bookController.getBookByCategory);
     router.get('/getdetailbook', tokenMiddleware, bookController.getDetailBook);
+    router.get('/modifierUser', tokenMiddleware, userMiddleware.modifierUsers);
     app.use('/api', router);
 };
 
