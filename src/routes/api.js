424f63c59e37aa3da39617a873f60bd3e30647e5
Bùi Nguyễn Nhật Tân <84611805+tanbui76@users.noreply.github.com>
2023-08-25 16:25:35 +0700

    tan

--- src/routes/api.js
+++ src/routes/api.js
@@ -10,7 +10,7 @@ let router = express.Router();
 const initAPIRoutes = (app) => {
     router.post('/register', authController.register);
     router.post('/login', authController.login);
-    router.get('/getbooks', tokenMiddleware, bookController.getAllBooks);
+    router.get('/getbooks', bookController.getAllBooks);
     router.get('/searchbooks', bookController.searchBook);
     router.get('/getbookbycategory', bookController.getBookByCategory);
     router.get('/getdetailbook', bookController.getDetailBook);
